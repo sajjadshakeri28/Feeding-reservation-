100e531eaa990087936cdff1dbb8518534b1a47b
mahdiaminee <mahdiamini8505@gmail.com>
2025-04-25 20:51:40 +0330

    class meal added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,76 @@
-#include <Iostream>
-
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
-    cout << "hello world!" << endl;
-}
+// وضعیت رزرو
+enum ReservationStatus { SUCCESS, FAILED, CANCELLED };
+
+class Meal {
+    private:
+        static int last_meal_id;
+        int meal_id;
+        string name;
+        string date;
+        MealType type;
+        int capacity;
+        float price;
+        vector<string> side_items;
+    public:
+    Meal(string name, string date, MealType type, int capacity, float price) :
+     name(name), date(date), type(type), capacity(capacity), price(price) {
+        meal_id = ++last_meal_id;
+     }
+
+        bool is_available() const{
+        return capacity > 0; }
+
+        void reserve_one(){
+            capacity --; }
+
+        void cancel_reservation(){
+            capacity ++; }
+
+        void update_price(float new_price){
+            price = new_price;
+        }
+
+        void add_side_item(const string& item){
+            side_items.push_back(item);
+        }
+
+        int get_meal_id() const{
+            return meal_id;
+        }
+
+        string get_name() const{
+            return name; }
+
+        string get_date() const{
+            return date; }
+
+        MealType get_type() const{
+            return type; }
+
+        float get_price() const{
+            return price;
+        }
+
+        const vector<string>& get_side_item() const {
+            return side_items;
+        }
+        void print() const {
+            cout << "Meal ID: " << meal_id << "\nName: " << name << "/nDate: " << date << "\nType: ";
+            if(type == BREAKFAST)
+            {
+                cout << "BreakFast";
+            }
+            else if(type == LUNCH){
+                cout << "Lunch";
+            }
+            else{
+                cout << "Dinner";
+            }
+            cout << "\nPrice: " << price << "\nCapacity: " << capacity << endl;
+        }
+    };
